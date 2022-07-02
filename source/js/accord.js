12398010400897951b2ebfc727e204039ba2dad0
Murad <abishlimurad@gmail.com>
2022-07-02 02:49:56 +0200

    01.07 fixes

--- source/js/accord.js
+++ source/js/accord.js
@@ -1,24 +1,24 @@
-// const acc = document.getElementsByClassName("faq__item-visible");
-// const arrows = document.getElementsByClassName('faq-arrow')
-// var i;
+const acc = document.getElementsByClassName("faq__item-visible");
+const arrows = document.getElementsByClassName('faq-arrow')
+var i;
 
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("collapse");
-//     const panel = this.nextElementSibling;
-//     if (panel.style.maxHeight){
-//       panel.style.maxHeight = null;
-//       panel.style.transition = 'all .4s ease'
-//       panel.style.opacity = '0'
-//       panel.style.visibility = 'hidden'
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("collapse");
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+      panel.style.transition = 'all .4s ease'
+      panel.style.opacity = '0'
+      panel.style.visibility = 'hidden'
 
-//     } else {
-//       panel.style.opacity = '1'
-//       panel.style.maxHeight = panel.scrollHeight + "px";
-//       panel.style.visibility = 'visible'
-//     }
-//   });
-// };
+    } else {
+      panel.style.opacity = '1'
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.visibility = 'visible'
+    }
+  });
+};
 
 // var visibleItems = Array.from(document.querySelectorAll('.faq__item'));
 // visibleItems.forEach(function (item) {
